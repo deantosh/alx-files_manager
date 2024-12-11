9c518bce4c0a2f41418928b644476cbd296965c2
deantosh <deantoshdaiddoh@gmail.com>
2024-12-10 23:23:28 -0800

    js style guide

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
-import { Router } from "express";
-import AppController from '../controllers/AppController.js';
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
 
 const router = Router();
 
 // define the routes
-router.get('/status', AppController.getStatus)
-router.get('/stats', AppController.getStats)
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-export default router
+export default router;
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
+import router from './routes/index';
 
 const app = express();
 
 app.use(express.json());
-
-// Add routes
-import router from './routes/index.js';
 app.use(router);
 
 const PORT = 5000;
