e22996c551739c202514940e92baaba3c7986c35
deantosh <deantoshdaiddoh@gmail.com>
2024-12-11 09:16:55 -0800

    task 4: add /users/me route handler

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,6 +2,7 @@
 import sha1 from 'sha1';
 import Queue from 'bull/lib/queue.js';
 import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 
 const userQueue = new Queue('email sending');
 
@@ -37,8 +38,22 @@ export default class UsersController {
   }
 
   static async getMe(req, res) {
-    const { user } = req;
+    const userToken = req.headers['X-Token'];
+    if (!userToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
 
-    res.status(200).json({ email: user.email, id: user._id.toString() });
+    // Create key
+    const key = `auth_${userToken}`;
+
+    // Get user id
+    const userId = await redisClient.get(key);
+
+    // Get user from mongodb
+    const usersCollection = dbClient.database.collection('users');
+    const user = await usersCollection.findOne({ id: userId });
+
+    // Return user email and id
+    return res.status(200).json({ email: user.email, userId });
   }
 }
